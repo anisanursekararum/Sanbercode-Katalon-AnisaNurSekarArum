ec4046ab401f06e544d13f9df258de9722a830d4
anisanursekararum <anisanursekararum@gmail.com>
2023-09-24 23:14:31 +0700

    product detail

---
+++ Object Repository/Menu/menuHome.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>menuHome</name>
+   <tag></tag>
+   <elementGuidId>fd22cd13-dad8-4466-a979-edeaba828823</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[@class='nav-item active']//a[@class='nav-link']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>

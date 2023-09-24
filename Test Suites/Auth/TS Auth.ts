26c4d75242dd1ee9a40493bf6ffca25a70db8807
anisanursekararum <anisanursekararum@gmail.com>
2023-09-24 17:30:45 +0700

    TS auth

---
+++ Test Suites/Auth/TS Auth.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Included TC's signup positive flow, signup negative</description>
+   <name>TS Auth</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c4afb84c-bc72-4695-b089-1e3dff9dafa2</testSuiteGuid>
+   <testCaseLink>
+      <guid>649a2f1b-fc29-4dca-b688-c0ed36b3d714</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Auth/TC Signup Negative</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>64481602-28d6-4421-89c4-2c9a356ba19c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Auth/TC Signup Positive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>67936378-a686-46c7-b886-404bc3f391ca</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e00a0e05-5ba6-4ff6-aabf-36ab9c0f4c81</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3ee94502-c763-4400-8d2b-e81b199d1cbf</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>

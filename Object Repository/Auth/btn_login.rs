98e234d9d0dab97efe67770ae495b1db5516263f
anisanursekararum <anisanursekararum@gmail.com>
2023-09-24 11:44:39 +0700

    add object repo

---
+++ Object Repository/Auth/btn_login.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_login</name>
+   <tag></tag>
+   <elementGuidId>0f23703e-8d98-4692-8232-6073495336a8</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
